cf5a5496db4c24b583dc2c80066582e88dbfdc62
R7558 <rupsanaworld1004@gmail.co>
2022-12-31 07:15:28 +0600

    added all

---
+++ string.js
@@ -0,0 +1,42 @@
+// let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// let length = text.length;
+// console.log(length);
+
+// let text= 'It\'s alright.';
+
+// let text = "The character \\ is called backslash.";
+// console.log(text);
+
+// let text = "Apple, Banana, Kiwi";
+// let part = text.slice(7, 13);
+
+// let text = "Apple, Banana, Kiwi";
+// let part = text.slice(7);
+
+// let text = "Apple, Banana, Kiwi";
+// let part = text.slice(-12);
+
+// let text = "Apple, Banana, Kiwi";
+// let part = text.slice(-12, -6);
+
+               // substr() is similar to slice().
+
+               let str = "Apple, Banana, Kiwi";
+let part = str.substr(7, 6);
+console.log(part);
+
+let text = "Please visit Microsoft!";
+let newText = text.replace("Microsoft", "W3Schools");
+console.log(newText);
+
+// let text1 = "Hello World!";
+// let text2 = text1.toUpperCase();
+// console.log(text2);
+
+// let text1 = "Hello World!";       // String
+// let text2 = text1.toLowerCase();
+
+let text1 = "Hello";
+let text2 = "World";
+let text3 = text1.concat(" ", text2);
+console.log(text3);
