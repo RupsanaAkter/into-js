3a149d3f0a9768c83fb5b680b15fae883e5bbbfe
R7558 <rupsanaworld1004@gmail.co>
2022-12-31 06:59:05 +0600

    added all

---
+++ math.js
@@ -0,0 +1,46 @@
+// let x = (0.2 * 10 + 0.1 * 10) / 10;
+// console.log(x);
+
+// let x = "10";
+// let y = "20";
+// let z = x + y;
+
+// let x = 10;
+// let y = "20";
+// let z = x + y;
+
+// let x = 10;
+// let y = 20;
+// let z = "The result is: " + x + y;
+
+// let x = "100";
+// let y = "10";
+// let z = x / y;
+
+// let x = "100";
+// let y = "10";
+// let z = x * y;
+
+// let x = "100";
+// let y = "10";
+// let z = x - y;
+
+// let x = "100";
+// let y = "10";
+// let z = x + y;
+
+
+// console.log(z);
+
+// let x = 100 / "Apple";
+
+// let a = 100 / "10";
+// console.log(a);
+// let x = 100 / "Apple";
+// console.log(isNaN(x));
+
+let x = NaN;
+let y = 5;
+let z = x + y;
+console.log(z);
+console.log(typeof(NaN));
